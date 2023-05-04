8b92632a2afb348cf99af0fd77abb0e94b72f3a3
‘Diontre <diontre01@gmail.com>
2023-05-04 00:20:58 -0500

    auth

--- thing/thing/views.py
+++ thing/thing/views.py
@@ -2,16 +2,17 @@ from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView
 from .models import Thing
 from .serializer import ThingSerializer
 from .permissions import isOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 
 class ThingList(ListAPIView):
-    permission_classes = (isOwnerOrReadOnly,)
+    permission_classes = (IsAuthenticated,)
     queryset = Thing.objects.all()
     serializer_class = ThingSerializer
 
 
 class ThingDetail(RetrieveUpdateDestroyAPIView):
-    permission_classes = (isOwnerOrReadOnly,)
+    permission_classes = (IsAuthenticated,)
     queryset = Thing.objects.all()
     serializer_class = ThingSerializer
 
